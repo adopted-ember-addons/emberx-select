20e303cf2f1995f463a6843a8cdee481b20528cc
Miguel <jmandrade0@gmail.com>
2015-03-18 17:43:46 +0000

    add safer array testing

--- addon/components/x-option.js
+++ addon/components/x-option.js
@@ -35,7 +35,7 @@ export default Ember.Component.extend({
    * @type Boolean
    */
   selected: Ember.computed('value', 'select.value', 'select.multiple', function() {
-    if (this.get('select.multiple') && this.get('select.value')) {
+    if (this.get('select.multiple') && Ember.isArray(this.get('select.value'))) {
       return this.get('select.value').contains(this.get('value'));
     } else {
       return this.get('value') === this.get('select.value');
