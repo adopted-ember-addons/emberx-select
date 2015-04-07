359c2ed312bd0fc948e6d4906d1514c6d7001122
Andreas Minnich <andreas.minnich@gmail.com>
2015-04-07 11:33:02 +0200

    add test that native select attributes are bound properly

--- tests/dummy/app/controllers/single.js
+++ tests/dummy/app/controllers/single.js
@@ -3,6 +3,12 @@ import Folks from 'dummy/mixins/folks';
 
 export default Ember.ObjectController.extend(Folks, {
   isDisabled: false,
+  isRequired: false,
+  hasAutofocus: false,
+  attrName: null,
+  attrForm: null,
+  attrSize: null,
+
   actions: {
     tagYouAreIt: function(object) {
       this.tagged = object;
