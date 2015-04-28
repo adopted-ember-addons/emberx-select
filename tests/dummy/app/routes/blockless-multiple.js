5636fea42f1a9508d30d08ec072b2fa1f3c6090f
Miguel <jmandrade0@gmail.com>
2015-04-28 19:14:30 +0100

    add tests

--- tests/dummy/app/routes/blockless-multiple.js
+++ tests/dummy/app/routes/blockless-multiple.js
@@ -3,11 +3,11 @@ import Folks from 'dummy/mixins/folks';
 
 export default Ember.Route.extend(Folks, {
   model: function() {
-    return Ember.A([this.get('bastion')]);
+    return Ember.A([this.get('bastion'), this.get('stanley')]);
   },
   setupController: function(controller, model) {
     this._super.apply(this, arguments);
     controller.set('it', model);
-    controller.set('folks', Ember.A([this.get('bastion'), this.get('stanley'), this.get('charles')]));
+    controller.set('folks', Ember.A([this.get('charles'), this.get('bastion'), this.get('stanley') ]));
   }
 });
