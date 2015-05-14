6c960d4d67feec85378ab885218c4d10746615c2
Robdel12 <robertdeluca19@gmail.com>
2015-05-14 17:49:44 -0500

    Stop using ES6 due to PhantomJS issues

--- addon/helpers/register-select-helper.js
+++ addon/helpers/register-select-helper.js
@@ -2,12 +2,14 @@ import Ember from 'ember';
 
 export default function() {
   Ember.Test.registerAsyncHelper('select', function(app, selector, text) {
-    const $el = app.testHelpers.findWithAssert(`${selector} option:contains("${text}")`);
+    var $el = app.testHelpers.findWithAssert(selector + "option:contains('" + text + "')");
 
     $el.each(function() {
-      Ember.run(() => {
-        this.selected = true;
-        Ember.$(this).trigger('change');
+      var _this = this;
+
+      Ember.run(function() {
+        _this.selected = true;
+        Ember.$(_this).trigger('change');
       });
     });
 
