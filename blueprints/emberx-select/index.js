107fdf372ecb64a1524bce4bb7d15eeffb4d5a20
Robdel12 <robertdeluca19@gmail.com>
2015-06-19 17:51:35 -0500

    Remove initalizer, use blueprint to add helper

--- blueprints/emberx-select/index.js
+++ blueprints/emberx-select/index.js
@@ -5,7 +5,27 @@ module.exports = {
 
   normalizeEntityName: function() {},
 
-  afterInstall: function(options) {
-    return this.insertIntoFile('tests/.jshintrc', '    "select",', { after: '"click",' + EOL});
+  afterInstall: function() {
+    // Import statement
+    var firstFile = 'tests/test-helper.js';
+    var firstText = "import registerSelectHelper from 'emberx-select/helpers/register-select-helper';";
+    var afterFirstText = "import resolver from './helpers/resolver';" + EOL;
+
+    // Execution of registration function
+    var secondFile = 'tests/test-helper.js';
+    var secondText = "registerSelectHelper();";
+    var afterSecondText = firstText + EOL;
+
+    return this.insertIntoFile(firstFile, firstText, { after: afterFirstText}).then(function() {
+      return this.insertIntoFile(secondFile, secondText, { after: afterSecondText});
+    }.bind(this)).then( function() {
+      return this.insertIntoFile('tests/.jshintrc', '    "select",', { after: '"click",' + EOL});
+    }.bind(this));
+
+    // return this.insertIntoFile(firstFile, firstText, { after: afterFirstText}).then(function() {
+    //   return this.insertIntoFile(secondFile, secondText, { after: afterSecondText});
+    // }.bind(this).then(function() {
+    //   return this.insertIntoFile('tests/.jshintrc', '    "select",', { after: '"click",' + EOL});
+    // }.bind(this)));
   }
 };
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
+import registerSelectHelper from 'emberx-select/helpers/register-select-helper';
+registerSelectHelper();
 
 
 setResolver(resolver);
