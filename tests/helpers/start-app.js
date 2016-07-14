58e654cf88b82af4ffc9a906fa0536b8692737c6
Robert DeLuca <robertdeluca19@gmail.com>
2016-07-14 14:35:51 -0500

    Update ember-cli to 2.6.2 (#122)

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,12 +4,12 @@ import config from '../../config/environment';
 import './get-component-by-id';
 
 export default function startApp(attrs) {
-  var application;
+  let application;
 
-  var attributes = Ember.merge({}, config.APP);
+  let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(function() {
+  Ember.run(() => {
     application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
