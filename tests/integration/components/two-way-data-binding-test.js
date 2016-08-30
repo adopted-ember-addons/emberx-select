ea9522537cd4a4eb0b6c46f283bb13f0f1989860
Brandon Hays <brandon.hays@gmail.com>
2016-08-30 09:48:08 -0600

    Fire action when one-way is true and no default value provided (#135)
    * fix value action firing when one-way is true and no default value provided
    
    * Small PR feedback changes
    
    - Remove unneeded action helper
    - Change value names to reflect they are the value and not the display
    - key.

--- tests/integration/components/two-way-data-binding-test.js
+++ tests/integration/components/two-way-data-binding-test.js
@@ -52,6 +52,36 @@ describeComponent(
           expect(this.blur).to.equal('dodge');
         });
       });
+    });
+
+    describe("default value of null with two way data binding disabled", function() {
+      beforeEach(function() {
+        this.set('make', null);
+        this.set('selectAction', (value) => {
+          this.set("make", value);
+          this.set("wasCalled", true);
+        });
+
+        this.render(hbs`
+          {{#x-select value=make one-way=true action=selectAction}}
+            {{#x-option value="fordValue" class="spec-ford-option"}}Ford{{/x-option}}
+            {{#x-option value="chevyValue"}}Chevy{{/x-option}}
+            {{#x-option value="dodgeValue" class="spec-dodge-option"}}Dodge{{/x-option}}
+          {{/x-select}}
+        `);
+      });
+
+      it("displays the first item in the list", function() {
+        expect(this.$('select option:selected').text()).to.equal("Ford");
+      });
+
+      it("sets the default value to the first element", function() {
+        expect(this.get('make')).to.equal("fordValue");
+      });
+
+      it("invokes the select action on init", function() {
+        expect(this.get("wasCalled")).to.equal(true);
+      });
 
     });
   }
