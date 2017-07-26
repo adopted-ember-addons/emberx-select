0b518a79d02194df7f7ff133d41e8b4d22ee3abe
Brian Runnells <brian.runnells@gmail.com>
2017-07-26 12:12:40 -0500

    ember-cli 2.14

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     // Add options here
     fsBranding: {
       enableAssets: true
@@ -16,7 +16,7 @@ module.exports = function(defaults) {
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
-   */
+  */
 
   app.import("bower_components/highlightjs/styles/github.css");
 
