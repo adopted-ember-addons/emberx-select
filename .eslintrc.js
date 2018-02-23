50cfc76ec0618f451f9ac60b6aeb6560317d5000
Elrick Ryan <elrickvm@gmail.com>
2018-02-23 12:15:08 -0500

    Lint for any jquery usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended'],
   env: {
     browser: true
   },
   plugins: [
+    'ember',
     "prefer-let"
   ],
   rules: {
+    "ember/no-jquery": 2,
     "prefer-let/prefer-let": 2
   }
 };
